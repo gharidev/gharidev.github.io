6ca7fdce0b9929f7d3eaee2068cfe305d9cc2a39
G Haridev <haridev.g@beinex.com>
2024-01-31 02:45:30 +0530

    GitHub pages updated.

--- script.js
+++ script.js
@@ -1,30 +1,116 @@
-const displayText = 'Coming soon!';
-
-const outputEl = document.querySelector('#textoutput');
-const cursor = document.querySelector('#cursor')
-
-const TYPE_INTERVAL = 200;
-const WAIT_INTERVAL = 2000;
-const CURSOR_INTERVAL = 500;
-var forward = true;
-
-function typer() {
-    var text = outputEl.textContent;
-    setTimeout(() => {
-        if (forward) {
-            text = outputEl.textContent += displayText[text.length];
-        } else {
-            text = outputEl.textContent = text.slice(0, text.length - 1);
+new Vue({
+    el: '#app',
+    data: {
+        history: [],
+        converter: new showdown.Converter(),
+        dotPicture: "",
+        input: '',
+        prevCount: 0,
+        commands: {
+            clear: {
+                description: 'Clear the terminal screen',
+                exec() {
+                    this.history = [];
+                }
+            },
+            help: {
+                description: 'Show available commands',
+                exec() {
+                    return Object.keys(this.commands).map(cmd => `${cmd} - ${this.commands[cmd].description}`).join('<br>');
+                }
+            },
+            "hello world": {
+                description: 'Hello world!',
+                value: "Hello World!",
+            },
+            instagram: {
+                description: "Instagram profile",
+                markdown: "instagram",
+            },
+            github: {
+                description: "GitHub profile",
+                markdown: "github",
+            },
+            about: {
+                description: "About me",
+                markdown: "about",
+            },
+            position: {
+                description: "Position in which I am currently working",
+                value: "Senior Software Engineer",
+            },
+            experience: {
+                description: "My current experience",
+                value: "3+",
+            },
+            languages: {
+                description: "Known Languages",
+                markdown: "languages",
+            }
         }
-        if (text.length == displayText.length || text.length == 0) {
-            forward = !forward;
+    },
+    methods: {
+        executeCommand() {
+            const cmd = this.input.trim();
+            if (cmd === '') return;
+
+            this.history.push({ type: 'prompt', content: cmd });
+
+            const commandLower = cmd.toLowerCase();
+
+            if (this.commands.hasOwnProperty(commandLower)) {
+                const command = this.commands[commandLower];
+                let output = null;
+                if ("exec" in command)
+                    output = command.exec.call(this);
+                else if ("template" in command)
+                    output = this.renderTemplate(command.template)
+                else if ("markdown" in command)
+                    output = this.renderMarkdown(command.markdown)
+                else
+                    output = command.value
+                if (output) this.history.push({ type: 'output', content: output });
+            } else {
+                this.history.push({ type: 'output', content: `Command not found: ${cmd}` });
+            }
+
+            this.clearInput();
+        },
+        previousCommand(){
+            const prompts = this.history.filter(h=>h.type === "prompt");
+            if (!prompts.length) return;
+            const prompt = prompts[prompts.length - this.prevCount - 1];
+            if (prompt === undefined) return;
+            this.input = prompt.content;
+            this.prevCount += 1;
+        },
+        clearInput(){
+            this.input = "";
+            this.prevCount = 0;
+        },
+        renderLink(title, link){
+            return `<a href="${link}">${title}</a>`
+        },
+        renderTemplate(elementId) {
+            return document.getElementById(elementId).innerHTML;
+        },
+        renderMarkdown(elementId){
+            return this.converter.makeHtml(document.getElementById(elementId).innerHTML.trim());
         }
-        typer();
-    }, (text.length == displayText.length || text.length == 0) ? WAIT_INTERVAL : TYPE_INTERVAL);
-}
-
-setInterval(() => {
-    cursor.classList.toggle('hide');
-}, CURSOR_INTERVAL);
-
-typer();
+    },
+    watch: {
+        input(val){
+            if (val === "") this.prevCount = 0;
+        }
+    },
+    mounted() {
+        this.$nextTick(() => {
+            this.$refs.input.focus();
+        });
+    },
+    async created(){
+        const res = await fetch("assets/profile.txt");
+        this.dotPicture = await res.text();
+    },
+    template: "#content",
+});
